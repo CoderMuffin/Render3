ad8b0fdc9168ad85edb0391711fb15ef9bd033ee
john <john@mooseandlemur.com>
2021-08-31 17:05:18 +0100

    Render3ConsoleTest Added

--- Render3/Core/Input.cs
+++ Render3/Core/Input.cs
@@ -1,9 +1,14 @@
 ﻿using System;
+using System.Collections.Generic;
 using Render3.Async;
 namespace Render3.Core
 {
     public class Input
     {
+        public bool this[char ch] {
+            get => down.Contains(ch);
+        }
+        private HashSet<char> down;
         public Flow inputFlow;
         public event Action<char> OnKey;
         public Input Initialize(Func<char> read)
@@ -11,14 +16,20 @@ namespace Render3.Core
             Flow.Go(() => {
                 while(true)
                 {
-                    OnKey?.Invoke(read());
+                    char ch = read();
+                    down.Add(ch);
+                    OnKey?.Invoke(ch);
                 }
             });
             return this;
         }
         public static char ReadConsole()
         {
-            return Console.ReadKey().KeyChar;
+            return Console.ReadKey(true).KeyChar;
+        }
+        public void Flush()
+        {
+            down.Clear();
         }
         //public Point2 screenMousePosition { get { return new Point2(System.Windows.Forms.Cursor.Position.X, System.Windows.Forms.Cursor.Position.Y); } set { System.Windows.Forms.Cursor.Position = value.ToWinFormsPoint(); } }
         //public Point2 mouseDelta { get { return new Point2(System.Windows.Forms.Cursor.Position.X, System.Windows.Forms.Cursor.Position.Y); } }
---
+++ Render3ConsoleTest/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Render3.Core;
+using Render3.Components;
+
+namespace Render3ConsoleTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
